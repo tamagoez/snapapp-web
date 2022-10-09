d98f41cc87a2e57c2f8bbc5435ca374e63a7d279
tamagoez <tabascoes0711@gmail.com>
2022-10-09 09:07:19 +0000

    Update welcome.tsx

--- pages/app/welcome.tsx
+++ pages/app/welcome.tsx
@@ -1,11 +1,19 @@
-import { userdata } from "../../scripts/auth";
+import { isAuthed, userdata } from "../../scripts/auth";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function welcome() {
+export default function Welcome() {
   const user = userdata();
+  const router = useRouter();
+  useEffect(() => {
+    if (!isAuthed) {
+      router.replace(`/app/login?flowto=${router.pathname}`);
+    }
+  }, [router]);
   return (
     <>
       <h1 className="text-4xl font-bold">Welcome!</h1>
-      <h2>Let&apos;s customize your account!</h2>ID: {user.id}
+      <h2>Let&apos;s customize your account!</h2>ID: {user!.id}
     </>
   );
 }
